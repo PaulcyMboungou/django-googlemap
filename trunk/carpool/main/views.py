40d3447559a152d7ea2bd350950297c50bc00344
esimorre@free.fr <esimorre@free.fr@1be8f269-f444-0410-85eb-137b948ff3f0>
2008-02-01 22:46:12 +0000

    add_dir request

--- trunk/carpool/main/views.py
+++ trunk/carpool/main/views.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from django.newforms import form_for_model, form_for_instance
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from models import UserDirLink, User
 
 def main(request, template='carpool_main.html'):
@@ -16,3 +17,6 @@ def main(request, template='carpool_main.html'):
                'gmap_global_include': 'global.js',
                'neighbour_dirs':None}
     return render_to_response(template, context)
+
+def add_dir(request):
+    return HttpResponse(request.GET['dir'])
