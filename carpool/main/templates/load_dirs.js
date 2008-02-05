669edcd1254c7a387915b7f181d7acb5bc329f5b
esimorre <esimorre@free.fr>
2008-02-05 23:02:00 +0000

    dir displayed as polylines

--- carpool/main/templates/load_dirs.js
+++ carpool/main/templates/load_dirs.js
@@ -2,10 +2,17 @@ gmap = map
 gdir = new GDirections(map);
 GEvent.addListener(gdir, "error", handleErrors);
 GEvent.addListener(gdir, "load", onGDirectionsLoad);
+var queryopts = {"getPolyline":true}
 
 {% for dir in db_dirs %}
-dir{{forloop.counter}} = new GDirections(map);
+dir{{forloop.counter}} = new GDirections();
 dir{{forloop.counter}}.{{ dir.js }}
+
+GEvent.addListener(dir{{forloop.counter}}, "load", function() {
+  var poly = dir{{forloop.counter}}.getPolyline();
+  gmap.addOverlay(poly);
+});
+
 {% endfor %}
 
 
