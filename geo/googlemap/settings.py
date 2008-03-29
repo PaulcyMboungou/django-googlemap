d5e54140c0ea461acf4f86be41e9fd82df82790a
esimorre <esimorre@free.fr>
2008-03-29 21:12:53 +0000

    settings_local for googlemap secret key

--- geo/googlemap/settings.py
+++ geo/googlemap/settings.py
@@ -2,3 +2,4 @@
 # url http://localhost:8000/
 GMAP_SECRET_KEY =  "ABQIAAAAiehfuX-3OQswVoGEWkc9zxQCULP4XOMyhPd8d_NrQQEO8sT8XBRpvrdNmehejY7_aw8JHlSLZh9DIA"
 IMAGES_PREFIX_URL = '/static'
+from settings_local import *
--- get_svn_carpool.sh
+++ get_svn_carpool.sh
@@ -9,6 +9,7 @@ cd carpooling/carpool
 PYTHONPATH=${HOME}/projects/carpooling:$HOME python manage.py syncdb
 cd ..
 cp -r static $HOME
+echo 'GMAP_SECRET_KEY = "ABQIAAAAiehfuX-3OQswVoGEWkc9zxTD2KCBnNxmqJQ9kY2vT-1IXhqJORSfXCdOLCCX_Ec1F8r9JcDKIIh_oA"' > settings_local.py
 
 cd $HOME
 ln -s projects/carpooling/carpool/fcgi_carpool carpool
